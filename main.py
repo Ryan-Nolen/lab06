53f252bf85f13887ad87ed950140c7308a586517
Ryan Nolen <ryan.nolen@ufl.edu>
2023-03-22 20:32:37 -0400

    Edited Program

--- main.py
+++ main.py
@@ -17,13 +17,18 @@ def encode():
 
 
 def decode():
-	pass
+	list_pass = []
+	for i in encoded_pass:
+		list_pass.append(int(i))
+	for j in range(len(list_pass[j] - 3
+	return "".join(str(value) for value in list_pass)
 
 
-while True
+while True:
+	menu()
 	option = int(input("Please enter an option: "))
 	if option == 1:
-		password = int(input("Please enter your password to encode: "))
+		password = input("Please enter your password to encode: ")
 		encoded_pass = encode()
 		print("Your password has been encoded and stored!")
 	elif option == 2:
